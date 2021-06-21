87f2a140be709cbd08a97903789ba6331240a041
GabrielBezerra01 <Gabriel.bezerra@ntsec.com.br>
2021-06-21 15:57:38 -0300

    O.O

--- Python/Bank/conta.py
+++ Python/Bank/conta.py
@@ -1,17 +1,46 @@
 class Conta:
     
+     
     def __init__(self, numero, titular,saldo,limite):
-        self.numero  = numero
-        self.titular = titular
-        self.saldo   = saldo
-        self.limite  = limite
+        self.__numero  = numero
+        self.__titular = titular
+        self.__saldo   = saldo
+        self.__limite  = limite
 
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def __pode_sacar(self, valor_para_sacar):
+        limite_disponivel = self.__saldo + self.__limite
+        return valor_para_sacar <= limite_disponivel
 
     def saca(self, valor):
-        self.saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print('O valor {}R$ ultrapassou o limite.'.format(valor))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
+    def transfere (self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+    @property
+    def titular(self):
+        return self.__titular
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+    
+    @staticmethod
+    def codigo_banco():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
