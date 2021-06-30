87dcc19292ec1c562d840189547bb35e5aea3b2d
GabrielRenan <gabriel.renan01@live.com>
2021-06-30 13:41:58 -0300

    final locadora and start string knowledge

--- Python/Locadora/modelo.py
+++ Python/Locadora/modelo.py
@@ -28,3 +28,39 @@ class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programa = programas
+
+    def __getitem__(self, item):
+        return self._programa[item]
+
+    @property
+    def __len__(self):
+        return len(self._programa)
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+
+listinha = [atlanta, vingadores, atlanta, tmep]
+
+playlist_fds = Playlist('FDS', listinha)
+
+print(f'Tamanho da Playlist: {playlist_fds}')
+
+for programa in playlist_fds:
+    print(programa)
